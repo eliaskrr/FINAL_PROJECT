ca28c96e9a07c6e4c734bba72b2294464e3640db
ilyasse <ilyasseessadak@gmail.com>
2022-03-27 22:21:25 +0200

    actualizando la ventana de carga

--- src/main/Library.java
+++ src/main/Library.java
@@ -1,3 +1,5 @@
+package main;
+import presentation.ScreenSplash;
 
 public class Library {
 
--- src/presentation/ScreenSplash.java
+++ src/presentation/ScreenSplash.java
@@ -1,3 +1,4 @@
+package presentation;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
