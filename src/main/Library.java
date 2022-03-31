b47988a51f947852cc95d024ecd90171adb00ca7
ilyasse <ilyasseessadak@gmail.com>
2022-03-31 22:35:17 +0200

    sustituyendo la anterior ventana de carga por una creada con windowbuilder

--- src/main/Library.java
+++ src/main/Library.java
@@ -1,19 +1,10 @@
 package main;
-import login.LoginWindow;
 import presentation.ScreenSplash;
 
 public class Library {
 
 	public static void main(String[] args) throws InterruptedException {
 		ScreenSplash screenSplash = new ScreenSplash();
-		LoginWindow login = new LoginWindow();
-		
-		// Inicia el hilo
-		//screenSplash.start();
-		// Espera a que finalice
-		screenSplash.join();
-		
-		// Abre la ventana del login
-		login.openWindow();
+		screenSplash.showWindow();
 	}
 }
--- src/presentation/ScreenSplash.java
+++ src/presentation/ScreenSplash.java
@@ -1,6 +1,5 @@
 package presentation;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
@@ -10,119 +9,101 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
-public class ScreenSplash extends Thread {
+import net.miginfocom.swing.MigLayout;
 
-	private JFrame frScreenSplash;
-	private JPanel pScreenSplash;
-	private JLabel lbAppIcon;
-	private JLabel lbLoading;
+public class ScreenSplash extends JFrame {
+
+	private JPanel pMain;
+	private JLabel lblApp; 
+	private JLabel lblLoading;
+	private JLabel lblAuthors;
 	private ImageIcon iconApp;
-
-	public void run() {
-		// Marco
-		frScreenSplash = new JFrame();
-		// Panel
-		pScreenSplash = new JPanel();
-		// Etiquetas
-		lbAppIcon = new JLabel("<html>ABC <br>Library</html>");
-		lbLoading = new JLabel("Cargando...");
-		
-		// Imagen escalada
-		iconApp = new ImageIcon(new ImageIcon("./resources/app_icon.png")
-				.getImage().getScaledInstance(80, 80, Image.SCALE_DEFAULT));
-		
-		// Crea la ventana de carga
-		createSplashScreen();
-	}
-
-	private void createSplashScreen() {
-		// Crea un nuevo marco
-		createFrame();
-		// Crea un nuevo panel
-		createPanel();
-		// Muestra el marco
-		frScreenSplash.setVisible(true);
-		// Cierra la ventana ("marco")
-		closeFrame();
-	}
-
-	private void createFrame() {
-		frScreenSplash.setSize(600, 300);
-		// Cambia el icono del programa
-		frScreenSplash.setIconImage(iconApp.getImage());
+	
+	public ScreenSplash() {
+		iconApp = new ImageIcon(new ImageIcon(".\\resources\\app_icon.png").getImage()
+				.getScaledInstance(80, 80, Image.SCALE_DEFAULT));
+		// Cierra la ejecución por defecto
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Dimensión del marco
+		setSize(600, 300);
+		// Elimina la barra de título
+		setUndecorated(true);
 		// Centra la ventana
-		frScreenSplash.setLocationRelativeTo(null);
-		// Elimina la barra del t�tulo
-		frScreenSplash.setUndecorated(true);
-		// Apaga el programa una vez cerrada la ventana
-		frScreenSplash.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
-	private void createPanel() {
-		// Tipo de layout
-		pScreenSplash.setLayout(new BorderLayout());
-		pScreenSplash.setBackground(Color.decode("#960018"));
-		
-		// Agrega los elementos al panel
-		addLabelsToPanel();
-
-		// Agrega el panel al marco
-		frScreenSplash.getContentPane().add(pScreenSplash);
+		setLocationRelativeTo(null);
+		setIconImage(iconApp.getImage());
 	}
 	
-	private void addLabelsToPanel() {
-		// Crea las etiquetas
-		createAppIconLabel();
+	public void showWindow() {
+		createPanel();
+		createAppLabel();
 		createLoadingLabel();
+		createAuthorsLabel();
+		setVisible(true);
+		closeWindow();
 	}
-
-	private void createAppIconLabel() {
-		Font fntAppIcon = new Font("Times New Roman", Font.PLAIN, 30);
-		
-		// Agrega la fuente
-		lbAppIcon.setFont(fntAppIcon);
-		// Color del texto
-		lbAppIcon.setForeground(Color.decode("#DACA84"));
-		// Agrega la imagen icono
-		lbAppIcon.setIcon(iconApp);
-		// Margen de separaci�n entre el texto y el icono
-		lbAppIcon.setIconTextGap(25);
-		// Alinea horizonalmente el panel
-		lbAppIcon.setHorizontalAlignment(JLabel.CENTER);
-		
-		/* 
-		 * Agrega la etiqueta al panel
-		 * y centra verticalmente el panel
-		 */
-		pScreenSplash.add(lbAppIcon, BorderLayout.CENTER);
+	
+	private void createPanel() {
+		pMain = new JPanel();
+		pMain.setBorder(new EmptyBorder(5, 5, 5, 5));
+		pMain.setBackground(Color.decode("#960018"));
+		setContentPane(pMain);
+		pMain.setLayout(new MigLayout("", "[590px]", "[249px][41px]"));
 	}
+	
+	private void createAppLabel() {
+		lblApp = new JLabel(
+				"<html>" + 
+				"ABC <br>Library" + 
+				"</html>");
+		lblApp.setFont(new Font("Times New Roman", Font.PLAIN, 30));
+		lblApp.setForeground(Color.decode("#DACA84"));
+		lblApp.setIcon(iconApp);
+		// Margen de separción entre el texto y la imagen
+		lblApp.setIconTextGap(25);
+		// Centra horizontalmente la etiquete
+		lblApp.setHorizontalAlignment(JLabel.CENTER);
 
+		// Agrega al panel
+		pMain.add(lblApp, "cell 0 0,grow");
+	}
+	
 	private void createLoadingLabel() {
-		Font fntLoading = new Font("Times New Roman", Font.PLAIN, 18);
-		
-		// Agrega la fuente
-		lbLoading.setFont(fntLoading);
-		// Color del texto
-		lbLoading.setForeground(Color.decode("#DACA84"));
+		lblLoading = new JLabel("Loading...");
+		lblLoading.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		lblLoading.setForeground(Color.decode("#DACA84"));
 		// Margénes de seraración
-		lbLoading.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 0));
+		lblLoading.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 0));
 		
-		/*
-		 *  Agrega la etiqueta al panel y 
-		 *  la pone en la esquina inferior izquierda
-		 */
-		pScreenSplash.add(lbLoading, BorderLayout.PAGE_END);
-		
+		// Agrega al panel
+		pMain.add(lblLoading, "flowx,cell 0 1,growx,aligny top");
 	}
+	
 
-	private void closeFrame() {
+	private void createAuthorsLabel() {
+		lblAuthors = new JLabel(
+				"<html>" + 
+				"Creado por:<br>" + 
+				"&emsp;&emsp;Ilyasse Essadak Samaali<br>" +
+				"&emsp;&emsp;Jose María Sandoval Cerezo<br>" + 
+				"&emsp;&emsp;Diego Hoyos Puche" + 
+				"</html>");
+		lblAuthors.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblAuthors.setForeground(Color.decode("#DACA84"));
+		
+		// Agrega al panel
+		pMain.add(lblAuthors, "cell 0 1 1 2,alignx right,aligny bottom");
+	}
+	
+	private void closeWindow() {
 		try {
-			this.sleep(2000);
+			Thread.sleep(2000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		frScreenSplash.dispose();
+		dispose();
 	}
+	
 }
